40d7fc9054fbd7bc4180ef6a30d59e73dacbb5a3
yarencheng <yarencheng@gmail.com>
2015-06-10 19:46:51 +0800

    initial project

---
+++ src/main/java/net/arenx/YourFirstAPI.java
@@ -0,0 +1,8 @@
+package net.arenx;
+
+/**
+  * Add your first API methods in this class, or you may create another class. In that case, please
+  * update your web.xml accordingly.
+ **/
+public class YourFirstAPI {
+}
