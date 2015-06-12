e60297947fefcc4229b1f4decb9e3629bf695ce5
Aren Chen <yarencheng@gmail.com>
2015-06-13 02:03:00 +0800

    Endpoint initial
    - Build OK
    - Can upload to AppEngine
    - https://net-arenx-candidate2016.appspot.com/_ah/api/explorer

--- src/main/java/net/arenx/YourFirstAPI.java
+++ src/main/java/net/arenx/YourFirstAPI.java
@@ -1,8 +1,28 @@
 package net.arenx;
 
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.config.Named;
+
 /**
   * Add your first API methods in this class, or you may create another class. In that case, please
   * update your web.xml accordingly.
  **/
+@Api(name = "myApi",
+version = "v1",
+namespace = @ApiNamespace(ownerDomain = "helloworld.example.com",
+                           ownerName = "helloworld.example.com",
+                           packagePath=""))
 public class YourFirstAPI {
+	@ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+        response.dada="your "+name;
+        return response;
+    }
+	
+	static class MyBean{
+		public String dada;
+	}
 }
